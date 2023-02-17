f6de07cb3f9f84ac77b23a81066eb7d198fb0874
gvarys <johnnyzaev@gmail.com>
2023-02-17 11:21:39 +0300

    Rocket now have Movement script and Rigidbody

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,12 +4,21 @@ public class Movement : MonoBehaviour
 {
     private void Update()
     {
-        ProcessMovement();
+        ProcessThrust();
+        ProcessRotation();
     }
 
-    private void ProcessMovement()
+    private void ProcessThrust()
     {
         if (Input.GetKeyDown(KeyCode.Space))
             Debug.Log("Space");
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+            Debug.Log("Left");
+        if (Input.GetKey(KeyCode.D))
+            Debug.Log("Right");
     }
 }
